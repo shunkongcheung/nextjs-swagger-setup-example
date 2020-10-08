b23c4ae85f76da584aeb650ab59a75504cbd8816
Shun Cheung <shunkongcheung@gmail.com>
2020-10-08 18:29:08 -0400

    feat(hello.ts): add type

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,6 +1,10 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { NextApiRequest, NextApiResponse } from 'next'
 
-export default (req, res) => {
+interface Data {
+	name:string
+}
+
+export default (req:NextApiRequest, res:NextApiResponse<Data>) => {
   res.statusCode = 200
   res.json({ name: 'John Doe' })
 }
