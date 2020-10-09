ea3d9e3b6796dc9af7173c8a6f027cdbe5fc433b
Shun Cheung <shunkongcheung@gmail.com>
2020-10-09 01:25:18 -0400

    feat(hello): add swagger

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,10 +1,47 @@
-import { NextApiRequest, NextApiResponse } from 'next'
+import { NextApiRequest, NextApiResponse } from "next";
+
+/**
+ * @swagger
+ *
+ * components:
+ *   hello:
+ *     HelloResponse:
+ *       type: object
+ *       properties:
+ *         name:
+ *           type: string
+ *
+ * tags:
+ *   - name: Hello
+ *     description: A sample of API Group 
+ *
+ * /hello:
+ *   get:
+ *     description: A test API
+ *     tags: [Hello]
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: name
+ *         in: query
+ *         required: true
+ *         type: string
+ *         description: Echo this name
+ *     responses:
+ *       201:
+ *         description: Sample response
+ *         schema:
+ *           $ref: '#/components/hello/HelloResponse'
+
+ *
+ */
 
 interface Data {
-	name:string
+  name: string;
 }
 
-export default (req:NextApiRequest, res:NextApiResponse<Data>) => {
-  res.statusCode = 200
-  res.json({ name: 'John Doe' })
-}
+export default (req: NextApiRequest, res: NextApiResponse<Data>) => {
+  const { name = "John Doe" } = req.query;
+  res.statusCode = 200;
+  res.json({ name });
+};
