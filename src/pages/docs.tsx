aee4449da711d9e6de7414ffe1212e70d149af2b
Shun Cheung <shunkongcheung@gmail.com>
2020-10-09 16:07:27 -0400

    feat(docs): redirect to 404g

--- src/pages/docs.tsx
+++ src/pages/docs.tsx
@@ -1,9 +1,28 @@
-import { NextPage } from "next";
+import { useEffect } from "react";
+import { GetStaticProps, InferGetStaticPropsType, NextPage } from "next";
+import { useRouter } from "next/router";
 
 import SwaggerUI from "swagger-ui-react";
 import "swagger-ui-react/swagger-ui.css";
 
-const Home: NextPage = () => {
+export const getStaticProps: GetStaticProps = async () => {
+  const { NODE_ENV } = process.env;
+
+  const isDev = NODE_ENV === "development";
+  return {
+    props: { isDev },
+  };
+};
+
+const Docs: NextPage = ({
+  isDev,
+}: InferGetStaticPropsType<typeof getStaticProps>) => {
+  const router = useRouter();
+  useEffect(() => {
+    if (isDev) router.push("/404");
+  }, [isDev]);
+
   return <SwaggerUI url="/swagger.json" />;
 };
-export default Home;
+
+export default Docs;
